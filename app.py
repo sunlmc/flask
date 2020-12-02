73c126fa48fa0d34784873429bed1734c39b1895
sunshine <lmc1056@wo.cn>
2020-12-02 14:37:26 +0800

    first web test page!

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask,url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Welcome to lmc web page!'
+@app.route('/index')
+@app.route('/home')
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Welcome to %s web page!</h1>' % name
+
+@app.route('/test')
+def test_url_for():
+	print(url_for('user',name='lmc'))
+	print(url_for('user',name='lly'))
+	print(url_for('test_url_for'))
+	print(url_for('test_url_for',num=2))
+	return '<h2>Test page!</h2>'
+
